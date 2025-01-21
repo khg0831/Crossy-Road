a45654394fcc07f17f88ef7d9293375a960efd9e
jucom0831 <jucom0831@gmail.com>
2025-01-21 17:36:37 +0900

    플레이어 이동 구현 중

--- Assets/Assets/Script/PlayerMove.cs
+++ Assets/Assets/Script/PlayerMove.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float startTouchTime;
+    private Vector2 stratTouchPos;
+    private float endTouchTime;
+    private float maxSwipeTime = 2f;
+    private int currentTouchCnt;
+    private float PressTime = 1f;
 
-    // Update is called once per frame
-    void Update()
+    public bool IsTouch { get; private set; }
+    public bool Tab { get; private set; }
+    public bool Press { get; private set; }
+    public Vector2 Swipe { get; private set; }
+
+    private Touch touch;
+
+    public float minSwipeDistance = 0.25f;
+    public float minSwipeDistancePixels;
+
+    public void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startTouchTime = Time.time;
+                    stratTouchPos = touch.position;
+                    if (Input.touchCount == 1)
+                        currentTouchCnt++;
+                    else
+                        currentTouchCnt = 0;
+                    IsTouch = true;
+                    break;
+                case TouchPhase.Moved:
+                case TouchPhase.Stationary:
+                    if (Time.time - startTouchTime > PressTime)
+                        Press = true;
+                    break;
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+
+                    var direction = stratTouchPos - touch.position;
+                    var distance = direction.magnitude;
+
+                    if (Time.time - startTouchTime < maxSwipeTime &&
+                        distance > minSwipeDistancePixels)
+                    {
+                        Swipe = direction.normalized;
+                    }
+                    endTouchTime = Time.time;
+                    break;
+            }
+
+        }
     }
 }
+
